c53f2962834fb785f254faf6e385476e681d3b06
Khush Patel <61388622+khushpatelmd@users.noreply.github.com>
2022-01-17 18:07:45 -0600

    Update engine_128_T1.py

--- training/engine_128_T1.py
+++ training/engine_128_T1.py
@@ -248,7 +248,7 @@ val_dataloader = torch.utils.data.DataLoader(
 dir_name = "T1_128"
 
 # initiaing the model
-AE_model = engine_AE(0.0013182567385564075)
+AE_model = engine_AE(0.003311311214825908)
 
 # learning rate monitor as using scheduler
 lr_monitor = LearningRateMonitor(logging_interval="epoch")
